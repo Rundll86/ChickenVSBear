7e5a8861ec26476d188952f4445426805d3895f3
陨落基围虾 <3161880837@qq.com>
2025-08-26 18:15:01 +0800

    feat: 删除FeedCard场景，重构Feed类，更新UI组件

--- scripts/Structs/Feed.gd
+++ scripts/Structs/Feed.gd
@@ -1,3 +1,15 @@
 @tool
 extends PanelContainer
 class_name Feed
+
+@export var avatarTexture: Texture2D = null
+@export var displayName: String = "未命名饲料"
+@export var fields: Array[FieldStore.Entity] = []
+@export var values: Array[float] = []
+@export var costs: Array[ItemStore.ItemType] = []
+@export var counts: Array[int] = []
+
+@onready var avatar: TextureRect = $"%avatar"
+@onready var nameLabel: Label = $"%name"
+@onready var fieldsBox: VBoxContainer = $"%fields"
+@onready var costsBox: VBoxContainer = $"%costs"
