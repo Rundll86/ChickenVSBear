ae77d624b1687dec630936fdfdf4634c072f6fd4
陨落基围虾 <3161880837@qq.com>
2025-08-28 07:48:21 +0800

    fix(子弹): 使用lerp_angle平滑旋转并调整追踪速度
    修改BigLaser和Diamond子弹的旋转逻辑，使用lerp_angle实现平滑过渡
    调整Diamond子弹的追踪速度系数从0.1提高到0.2

--- scripts/Contents/Bullets/BigLaser.gd
+++ scripts/Contents/Bullets/BigLaser.gd
@@ -2,7 +2,7 @@ extends BulletBase
 class_name BigLaser
 
 func ai():
-	rotation = (get_global_mouse_position() - position).angle()
+	rotation = lerp_angle(rotation, ((get_global_mouse_position() - position).angle()), 0.05)
 	position = launcher.texture.global_position
 func applyDot():
 	hitbox.disabled = true
