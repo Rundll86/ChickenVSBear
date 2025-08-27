23d9020f264fbda734192436a009ced4ce20d1be
陨落基围虾 <3161880837@qq.com>
2025-08-27 13:01:01 +0800

    refactor: 移除不必要的_ready函数，优化beforeOpen逻辑以直接显示和处理喂食项

--- scripts/Contents/Panels/MakeFeed.gd
+++ scripts/Contents/Panels/MakeFeed.gd
@@ -4,10 +4,6 @@ extends FullscreenPanelBase
 @onready var avaliableFeeds: Node = $"%avaliableFeeds"
 @onready var feedCards: HBoxContainer = $"%feedcards"
 
-func _ready():
-	for i in avaliableFeeds.get_children():
-		i.hide()
-
 func beforeOpen():
 	var feeds: Array[Feed] = []
 	for i in avaliableFeeds.get_children():
@@ -17,12 +13,14 @@ func beforeOpen():
 		afterClose()
 		feeds.shuffle()
 		for i in range(3):
-			var feed = feeds[i]
-			var cloned = feed.duplicate() as Feed
-			cloned.show()
-			feedCards.add_child(cloned)
-			if cloned.allHad(UIState.player):
+			var feed = feeds[i] as Feed
+			feed.show()
+			avaliableFeeds.remove_child(feed)
+			feedCards.add_child(feed)
+			if feed.allHad(UIState.player):
 				allHad = true
 func afterClose():
 	for i in feedCards.get_children():
+		i.hide()
 		feedCards.remove_child(i)
+		avaliableFeeds.add_child(i)
