a3ae7ae8875c374684cba8367c091738cdf4407f
陨落基围虾 <3161880837@qq.com>
2025-08-27 15:41:52 +0800

    fix: 修正Diamond子弹的旋转计算，确保追踪效果更平滑；更新Chick角色的生成参数

--- scripts/Contents/Bullets/Diamond.gd
+++ scripts/Contents/Bullets/Diamond.gd
@@ -4,6 +4,6 @@ class_name Diamond
 const traceTime = 1000
 
 func ai():
-	rotation = lerp_angle(rotation, position.angle_to_point(launcher.currentFocusedBoss.position), 0.2 * (traceTime - timeLived()))
+	rotation = lerp_angle(rotation, position.angle_to_point(launcher.currentFocusedBoss.position), 0.2 * ((traceTime - timeLived()) / traceTime))
 	canDamageSelf = !(timeLived() >= traceTime)
 	forward(Vector2.from_angle(rotation))
