9748a84c626b26589a047298a1c23fc50e1ae205
陨落基围虾 <3161880837@qq.com>
2025-08-29 15:17:34 +0800

    feat: 调整饲料属性和数值配置
    - 更新多种饲料的字段和数值配置
    - 修改FieldStore中实体最大值限制
    - 简化Dango饲料的属性和成本

--- scripts/Tools/FieldStore.gd
+++ scripts/Tools/FieldStore.gd
@@ -76,11 +76,11 @@ static var entityMapType = {
 	Entity.ENERGY_REGENERATION: DataType.PERCENT,
 }
 static var entityMaxValueMap = {
-	Entity.CRIT_RATE: 1,
-	Entity.PENERATE: 1,
-	Entity.PENARATION_RESISTANCE: 1,
-	Entity.PRICE_REDUCTION: 0.8,
-	Entity.DROP_APPLE_RATE: 0.5,
+	Entity.CRIT_RATE: 0.8,
+	Entity.PENERATE: 0.8,
+	Entity.PENARATION_RESISTANCE: 0.5,
+	Entity.PRICE_REDUCTION: 0.4,
+	Entity.DROP_APPLE_RATE: 0.4,
 	Entity.FEED_COUNT_SHOW: 6
 }
 static var entityApplier = {
