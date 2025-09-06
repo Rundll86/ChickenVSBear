e06a90104deefcb7287c99a1d6c7d275d3d0f2f8
陨落基围虾 <3161880837@qq.com>
2025-09-06 17:33:19 +0800

    feat[角色]: 添加Rooster角色及相关武器资源
    新增Rooster角色脚本及LGBT武器UI组件，完善角色战斗系统资源。

--- scripts/Contents/Characters/Rooster.gd
+++ scripts/Contents/Characters/Rooster.gd
@@ -17,9 +17,13 @@ func ai():
 		tryAttack(0)
 	elif Input.is_action_pressed("attack2"):
 		tryAttack(1)
+	elif Input.is_action_pressed("smallSkill"):
+		tryAttack(2)
+	elif Input.is_action_pressed("superSkill"):
+		tryAttack(3)
 	for i in range(3):
-		if Input.is_action_pressed("skill" + str(i)):
-			tryAttack(2 + i)
+		if Input.is_action_pressed("cardSkill" + str(i)):
+			tryAttack(4 + i)
 	if Input.is_action_just_pressed("sprint"):
 		trySprint()
 	if Input.is_action_just_pressed("heal"):
