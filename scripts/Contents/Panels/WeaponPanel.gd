bbe989a40e186edb233a9f5c1ecd43d73352740c
陨落基围虾 <3161880837@qq.com>
2025-09-06 11:36:02 +0800

    fix(WeaponPanel): 修复武器面板切换时显示状态问题
    在打开面板前显示武器，关闭面板后隐藏武器，确保武器状态正确切换

--- scripts/Contents/Panels/WeaponPanel.gd
+++ scripts/Contents/Panels/WeaponPanel.gd
@@ -5,9 +5,11 @@ extends FullscreenPanelBase
 
 func beforeOpen():
 	for weapon in UIState.player.weapons:
+		weapon.show()
 		UIState.player.weaponStore.remove_child(weapon)
 		box.add_child(weapon)
 func afterClose():
 	for weapon in box.get_children():
+		weapon.hide()
 		box.remove_child(weapon)
 		UIState.player.weaponStore.add_child(weapon)
