acadf0a5f5f72964886a9e3643e137fdea832f3a
陨落基围虾 <3161880837@qq.com>
2025-09-06 18:01:57 +0800

    fix: 调整LGBT武器的能量需求为50，优化能量管理逻辑

--- scripts/Statemachine/EntityBase.gd
+++ scripts/Statemachine/EntityBase.gd
@@ -90,8 +90,6 @@ var cooldownTimer = CooldownTimer.new()
 var weapons: Array[Weapon] = []
 
 func _ready():
-	health = fields.get(FieldStore.Entity.MAX_HEALTH)
-	energy = fields.get(FieldStore.Entity.MAX_ENERGY)
 	register()
 	var selfStatebar: EntityStateBar = $"%statebar"
 	if isBoss:
@@ -99,7 +97,6 @@ func _ready():
 	else:
 		statebar = selfStatebar
 		statebar.entity = self
-		statebar.forceSync()
 	if isPlayer():
 		for i in weaponStore.get_children():
 			i.hide()
@@ -121,6 +118,10 @@ func _ready():
 	else:
 		currentFocusedBoss = get_tree().get_nodes_in_group("players")[0]
 		applyLevel()
+	health = fields.get(FieldStore.Entity.MAX_HEALTH)
+	energy = fields.get(FieldStore.Entity.MAX_ENERGY)
+	if is_instance_valid(statebar):
+		statebar.forceSync()
 	healthChanged.connect(
 		func(newHealth):
 			if is_instance_valid(statebar):
