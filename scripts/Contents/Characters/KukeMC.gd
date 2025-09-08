afc27775914c02eee083a1722f5d740885829cce
陨落基围虾 <3161880837@qq.com>
2025-09-08 22:14:05 +0800

    feat(角色): 添加KukeMC的AI逻辑和轮廓着色器
    实现KukeMC的AI行为，包括子弹检测和Boss伤害逻辑
    添加新的轮廓着色器用于角色渲染
    在测试场景中新增角色和着色器演示

--- scripts/Contents/Characters/KukeMC.gd
+++ scripts/Contents/Characters/KukeMC.gd
@@ -1,2 +1,15 @@
 extends EntityBase
 class_name KukeMC
+func ai():
+	for bullet in get_tree().get_nodes_in_group("bullet"):
+		if (
+			bullet is LGBTBullet and
+			bullet.position.distance_to(self.position) < 100
+		):
+			bullet.tryDestroy()
+	for entity in get_tree().get_nodes_in_group("bosses"):
+		if (
+			entity.name == "FurryR" and
+			entity.position.distance_to(self.position) < 100
+		):
+			entity.takeDamage(114514)
