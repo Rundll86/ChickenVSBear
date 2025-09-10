2c80fd2f7c616a73db24e6fb175e80d4e05f2e38
陨落基围虾 <3161880837@qq.com>
2025-09-10 22:31:47 +0800

    feat(武器): 调整武器属性和描述
    修改武器升级消耗参数，更新LGBT和VectorStar武器的显示名称和描述文本，优化物品掉落数量计算逻辑

--- scripts/Statemachine/EntityBase.gd
+++ scripts/Statemachine/EntityBase.gd
@@ -255,7 +255,7 @@ func tryDie(by: BulletBase):
 		var item = drops[drop]
 		var count = ceil(randf_range(dropCounts[drop].x, dropCounts[drop].y))
 		for i in range(count):
-			ItemDropped.generate(item, 1, position + MathTool.randv2_range(GameRule.itemDroppedSpawnOffset))
+			ItemDropped.generate(item, randi_range(0, int(sqrt(count))), position + MathTool.randv2_range(GameRule.itemDroppedSpawnOffset))
 	if MathTool.rate(
 		GameRule.appleDropRate +
 		by.launcher.fields.get(FieldStore.Entity.DROP_APPLE_RATE) +
