944c76453a9437f2a3d94d81289865153dfe908a
陨落基围虾 <3161880837@qq.com>
2025-09-14 14:10:07 +0800

    feat(角色): 增强KukeMC角色属性和攻击模式
    - 增加KukeMC的最大生命值和移动速度
    - 调整攻击冷却时间和射击偏移量
    - 实现跟随BOSS的AI行为
    - 修改紫水晶子弹的生成数量和位置
    - 为武器节点添加normal锚点

--- scripts/Contents/Characters/KukeMC.gd
+++ scripts/Contents/Characters/KukeMC.gd
@@ -1,10 +1,12 @@
 extends EntityBase
 class_name KukeMC
 func register():
-	fields[FieldStore.Entity.MAX_HEALTH] = 1000
-	fields[FieldStore.Entity.OFFSET_SHOOT] = 45
-	attackCooldownMap[0] = 500
+	fields[FieldStore.Entity.MAX_HEALTH] = 2500
+	fields[FieldStore.Entity.OFFSET_SHOOT] = 15
+	fields[FieldStore.Entity.MOVEMENT_SPEED] = 0.5
+	attackCooldownMap[0] = 2000
 func ai():
+	PresetEntityAI.follow(self, currentFocusedBoss, 500)
 	for bullet in get_tree().get_nodes_in_group("bullets"):
 		if (
 			bullet is LGBTBullet and
@@ -14,5 +16,6 @@ func ai():
 	tryAttack(0)
 func attack(type):
 	if type == 0:
-		for i in randi_range(3, 8):
+		for i in randi_range(8, 16):
 			BulletBase.generate(preload("res://components/Bullets/PurpleCrystal.tscn"), self, findWeaponAnchor("normal"), position.angle_to_point(currentFocusedBoss.position))
+			await TickTool.millseconds(randi_range(10, 50))
