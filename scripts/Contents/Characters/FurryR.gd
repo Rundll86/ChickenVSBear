27cf8d85e6810fe25d6e2da67c23127b835c4570
陨落基围虾 <3161880837@qq.com>
2025-09-14 13:57:53 +0800

    refactor(Characters): 移除FurryR角色并增强KukeMC能力
    移除未使用的FurryR角色相关文件
    增强KukeMC的攻击能力，增加紫色水晶子弹攻击
    调整KukeMC的碰撞检测范围并优化状态栏位置
    注释掉测试波次中的无关角色

--- scripts/Contents/Characters/FurryR.gd
+++
@@ -1,6 +0,0 @@
-extends EntityBase
-class_name FurryR
-
-func register():
-	fields[FieldStore.Entity.MAX_HEALTH] *= 0.05
-	fields[FieldStore.Entity.DAMAGE_MULTIPILER] = 999.0
--- scripts/Contents/Characters/KukeMC.gd
+++ scripts/Contents/Characters/KukeMC.gd
@@ -2,10 +2,17 @@ extends EntityBase
 class_name KukeMC
 func register():
 	fields[FieldStore.Entity.MAX_HEALTH] = 1000
+	fields[FieldStore.Entity.OFFSET_SHOOT] = 45
+	attackCooldownMap[0] = 500
 func ai():
 	for bullet in get_tree().get_nodes_in_group("bullets"):
 		if (
 			bullet is LGBTBullet and
-			bullet.position.distance_to(self.position) < 200
+			bullet.position.distance_to(self.position) < 200 # 酷可mc会去摧毁200半径以内的七彩飞星
 		):
 			bullet.tryDestroy()
+	tryAttack(0)
+func attack(type):
+	if type == 0:
+		for i in randi_range(3, 8):
+			BulletBase.generate(preload("res://components/Bullets/PurpleCrystal.tscn"), self, findWeaponAnchor("normal"), position.angle_to_point(currentFocusedBoss.position))
