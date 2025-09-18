c69d7ab50f510d0106df02fbe17b24d9107af8a9
陨落基围虾 <3161880837@qq.com>
2025-09-18 22:13:10 +0800

    fix(BossAttack): 调整重型紫水晶子弹行为和粒子效果
    修改子弹初始速度和加速度方向，移除无用粒子效果代码
    调整粒子系统参数并增加粒子数量，优化视觉效果
    修复碰撞检测逻辑，确保仅在激活时造成伤害

--- scripts/Contents/Bullets/BossAttack/KukeMC/HeavyCrystal.gd
+++ scripts/Contents/Bullets/BossAttack/KukeMC/HeavyCrystal.gd
@@ -1,20 +1,19 @@
 extends BulletBase
 
-@onready var trail: GPUParticles2D = $"%trail"
 @onready var track: Node2D = $"%track"
 
 var readyTime: float = 1000
 
 func register():
-	speed = -10
-func spawn():
-	trail.emitting = false
+	speed = 10
 func ai():
 	if timeLived() < readyTime:
 		PresetBulletAI.lockLauncher(self, launcher, true)
 		rotation = launcher.position.angle_to_point(launcher.currentFocusedBoss.position)
+		hitbox.disabled = true
 	else:
+		damage = speed / 4
 		PresetBulletAI.forward(self, rotation)
-		speed += 2
-		trail.emitting = true
+		speed *= 1.2
 		track.visible = false
+		hitbox.disabled = false
