1b320f3302cc428301985794e41402b52b797915
陨落基围虾 <3161880837@qq.com>
2025-09-21 22:36:45 +0800

    feat(ChainGun): 为链式机枪添加子弹数量属性并调整子弹生成逻辑
    修改链式机枪的武器脚本、场景配置和子弹脚本，新增count属性控制子弹数量
    子弹生成时根据count值调整位置分布，同时更新UI描述文本

--- scripts/Contents/Bullets/ChainGun.gd
+++ scripts/Contents/Bullets/ChainGun.gd
@@ -1,9 +1,14 @@
 extends BulletBase
 
+var count: int = 1
+
 @onready var anchor: Node2D = $"%anchor"
 
 func spawn():
-	for i in BulletBase.generate(ComponentManager.getBullet("PurpleCrystalSmall"), launcher, anchor.global_position, rotation):
-		i.damage = damage
+	for j in count:
+		for i in BulletBase.generate(ComponentManager.getBullet("PurpleCrystalSmall"), launcher, anchor.global_position, rotation):
+			i.damage = damage
+			var dir = Vector2.from_angle(i.rotation).rotated(deg_to_rad(-90))
+			i.global_position += dir * (count - j * 2) * 20 / 2
 func ai():
 	PresetBulletAI.lockLauncher(self, launcher, true)
--- scripts/Contents/Weapons/ChainGun.gd
+++ scripts/Contents/Weapons/ChainGun.gd
@@ -3,7 +3,9 @@ extends Weapon
 
 func update(to, origin, _entity):
 	origin["atk"] += 2 * to * soulLevel
+	origin["count"] += 0.25 * to * soulLevel
 	return origin
 func attack(entity: EntityBase):
 	for i in BulletBase.generate(ComponentManager.getBullet("ChainGun"), entity, entity.texture.global_position, (get_global_mouse_position() - entity.texture.global_position).angle()):
 		i.damage = readStore("atk")
+		i.count = floor(readStore("count"))
