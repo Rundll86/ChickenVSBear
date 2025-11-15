a4504ec2591c88132743b4d0823d272fa134a318
陨落基围虾 <3161880837@qq.com>
2025-11-16 07:42:42 +0800

    refactor(Bullets): 移除LGBTBullet中未使用的追踪逻辑
    简化子弹追踪逻辑，删除未使用的lastDistance和missedTarget变量及相关代码

--- scripts/Contents/Bullets/LGBTBullet.gd
+++ scripts/Contents/Bullets/LGBTBullet.gd
@@ -4,21 +4,14 @@ class_name LGBTBullet
 var tracer: EntityBase = null
 var maxTraceTime: float = 0
 var tracePower: float = 0
-var lastDistance: float = INF
-var missedTarget: bool = false
 
 func register():
 	speed = 1
 func ai():
 	speed *= 1.05
 	speed = clamp(speed, 0, 20)
-	if !missedTarget and is_instance_valid(tracer) and timeLived() < maxTraceTime:
+	if is_instance_valid(tracer) and timeLived() < maxTraceTime:
 		PresetBulletAI.trace(self, tracer.getTrackingAnchor(), tracePower)
 	PresetBulletAI.forward(self, rotation)
-	var currentDistance = position.distance_to(tracer.getTrackingAnchor())
-	if currentDistance > lastDistance:
-		missedTarget = true
-	else:
-		lastDistance = currentDistance
 func destroy(_b):
 	EffectController.create(ComponentManager.getEffect("LGBTBoom"), position).shot()
