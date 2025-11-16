b470fec115768c48326d6b76fd298277b907114a
陨落基围虾 <3161880837@qq.com>
2025-11-16 16:34:59 +0800

    fix: 调整游戏难度和平衡性
    将初始波数从10改为1以降低游戏初始难度
    减少猫角色的攻击冷却时间范围从3500-8000到2500-6000
    添加背景图片资源文件

--- scripts/Contents/Characters/Cat.gd
+++ scripts/Contents/Characters/Cat.gd
@@ -4,7 +4,7 @@ class_name Maodie
 func register():
 	fields[FieldStore.Entity.MAX_HEALTH] = 75
 	fields[FieldStore.Entity.MOVEMENT_SPEED] = randf_range(0.5, 0.8)
-	attackCooldownMap[0] = randi_range(3500, 8000)
+	attackCooldownMap[0] = randi_range(2500, 6000)
 	sprintMultiplier = randf_range(10, 35)
 func ai():
 	PresetEntityAI.follow(self, currentFocusedBoss)
