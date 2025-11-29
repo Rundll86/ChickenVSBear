9ac57e3afae8762798eced6219c4db2419bd9c25
陨落基围虾 <3161880837@qq.com>
2025-11-29 20:12:21 +0800

    fix(Weapons/Volcano): 调整火山武器伤害数值和描述
    更新火山武器的伤害计算参数，将dmg1-dmg5的伤害加成从0.01提升至0.02，并统一dmg5的加成比例。同时调整初始伤害数值和描述文本，使其与新的数值匹配。添加debugRebuild标志以便调试。

--- scripts/Contents/Weapons/Volcano.gd
+++ scripts/Contents/Weapons/Volcano.gd
@@ -3,11 +3,11 @@ extends Weapon
 
 func update(to: int, origin: Dictionary, _entity: EntityBase):
 	origin["rotate"] += 0.005 * to * soulLevel
-	origin["dmg1"] += 0.01 * to * soulLevel
-	origin["dmg2"] += 0.01 * to * soulLevel
-	origin["dmg3"] += 0.01 * to * soulLevel
-	origin["dmg4"] += 0.01 * to * soulLevel
-	origin["dmg5"] += 0.005 * to * soulLevel
+	origin["dmg1"] += 0.02 * to * soulLevel
+	origin["dmg2"] += 0.02 * to * soulLevel
+	origin["dmg3"] += 0.02 * to * soulLevel
+	origin["dmg4"] += 0.02 * to * soulLevel
+	origin["dmg5"] += 0.02 * to * soulLevel
 	origin["count"] = 1 * soulLevel
 	origin["atk"] += 1 * to * soulLevel
 	return origin
