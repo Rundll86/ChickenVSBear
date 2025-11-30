d869c2d44c2af7006cace1425ecbd60dee54c7d1
陨落基围虾 <3161880837@qq.com>
2025-11-30 16:36:40 +0800

    fix(Weapons/Volcano): 提高所有伤害等级的伤害加成系数
    将伤害加成系数从0.015提升至0.025以增强武器效果

--- scripts/Contents/Weapons/Volcano.gd
+++ scripts/Contents/Weapons/Volcano.gd
@@ -3,11 +3,11 @@ extends Weapon
 
 func update(to: int, origin: Dictionary, _entity: EntityBase):
 	origin["rotate"] += 0.005 * to * soulLevel
-	origin["dmg1"] += 0.015 * to * soulLevel
-	origin["dmg2"] += 0.015 * to * soulLevel
-	origin["dmg3"] += 0.015 * to * soulLevel
-	origin["dmg4"] += 0.015 * to * soulLevel
-	origin["dmg5"] += 0.015 * to * soulLevel
+	origin["dmg1"] += 0.025 * to * soulLevel
+	origin["dmg2"] += 0.025 * to * soulLevel
+	origin["dmg3"] += 0.025 * to * soulLevel
+	origin["dmg4"] += 0.025 * to * soulLevel
+	origin["dmg5"] += 0.025 * to * soulLevel
 	origin["count"] = 1 * soulLevel
 	origin["atk"] += 1 * to * soulLevel
 	return origin
