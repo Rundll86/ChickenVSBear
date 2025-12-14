06dc8bf79f4c1e4936e9f2a6813823a8e6ba6d97
陨落基围虾 <3161880837@qq.com>
2025-12-14 15:29:44 +0800

    feat(MTY): 增强MTY角色的攻击能力
    - 将"猛冲"技能更名为"撕咬"并增加生命周期
    - 为MTY添加新的攻击类型和冷却时间
    - 调整MTY在普通波次中的生成参数

--- scripts/Contents/Characters/MTY.gd
+++ scripts/Contents/Characters/MTY.gd
@@ -5,15 +5,17 @@ func register():
 	fields[FieldStore.Entity.MAX_HEALTH] = 400
 	fields[FieldStore.Entity.MOVEMENT_SPEED] = 0.9
 	attackCooldownMap[0] = 1500
+	attackCooldownMap[1] = 500
 	sprintMultiplier = 5
-func spawn():
-	BulletBase.generate(ComponentManager.getBullet("MTYSprint"), self, position, 0)
 func ai():
 	PresetEntityAI.follow(self, currentFocusedBoss)
 	tryAttack(0)
+	tryAttack(1)
 func attack(type: int):
 	if type == 0:
 		trySprint()
+	elif type == 1:
+		BulletBase.generate(ComponentManager.getBullet("MTYSprint"), self, position, 0)
 	return true
 func sprint():
 	var target = BulletTool.findClosetBulletCanDamage(position, get_tree(), self)
