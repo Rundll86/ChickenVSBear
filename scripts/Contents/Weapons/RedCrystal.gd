ce86c1247cc2c61b0c8ec9b8c7d45651c2bb8203
陨落基围虾 <3161880837@qq.com>
2025-12-17 22:21:24 +0800

    fix(Weapons/RedCrystal): 调整百分比加成系数从0.02降至0.001
    平衡性调整，降低红水晶武器的百分比加成效果

--- scripts/Contents/Weapons/RedCrystal.gd
+++ scripts/Contents/Weapons/RedCrystal.gd
@@ -6,7 +6,7 @@ func update(to: int, origin: Dictionary, _entity: EntityBase):
 	origin["atk"] += 5 * to * soulLevel
 	origin["radius"] += 1 * to * soulLevel
 	origin["count"] = 1 * soulLevel
-	origin["percent"] += 0.02 * to * soulLevel
+	origin["percent"] += 0.001 * to * soulLevel
 	return origin
 func attack(entity: EntityBase):
 	var weaponPos = entity.findWeaponAnchor("normal")
