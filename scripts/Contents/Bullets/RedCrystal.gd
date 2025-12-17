56e64ee6fd7b17d797b0b4cbbc8b7efb38ca381e
陨落基围虾 <3161880837@qq.com>
2025-12-17 22:23:37 +0800

    feat(武器): 增强红水晶武器爆炸效果
    修改红水晶武器爆炸时生成的水晶碎片数量逻辑，从固定数量改为1~N随机数量
    更新相关描述文本以反映新的随机数量机制

--- scripts/Contents/Bullets/RedCrystal.gd
+++ scripts/Contents/Bullets/RedCrystal.gd
@@ -3,6 +3,7 @@ class_name RedCrystalBullet
 
 var radius: float = 0
 var percent: float = 0
+var count: int = 0
 
 func register():
 	hitbox.shape = hitbox.shape.duplicate()
@@ -12,7 +13,8 @@ func ai():
 func destroy(_beacuseMap: bool):
 	hitbox.shape.radius = radius
 	EffectController.create(ComponentManager.getEffect("RedCrystalExplosion"), global_position).shot()
-	for bullet in BulletBase.generate(ComponentManager.getBullet("CrystalBlock"), launcher, position, deg_to_rad(randf_range(0, 360))):
-		if bullet is CrystalBlockBullet:
-			bullet.baseDamage = baseDamage * percent
+	for i in randi_range(1, count):
+		for bullet in BulletBase.generate(ComponentManager.getBullet("CrystalBlock"), launcher, position, deg_to_rad(randf_range(0, 360))):
+			if bullet is CrystalBlockBullet:
+				bullet.baseDamage = baseDamage * percent
 	await TickTool.millseconds(100)
--- scripts/Contents/Weapons/RedCrystal.gd
+++ scripts/Contents/Weapons/RedCrystal.gd
@@ -5,7 +5,7 @@ class_name RedCrystalWeapon
 func update(to: int, origin: Dictionary, _entity: EntityBase):
 	origin["atk"] += 5 * to * soulLevel
 	origin["radius"] += 1 * to * soulLevel
-	origin["count"] = 1 * soulLevel
+	origin["count"] = 2 + 1 * soulLevel
 	origin["percent"] += 0.001 * to * soulLevel
 	return origin
 func attack(entity: EntityBase):
@@ -14,4 +14,6 @@ func attack(entity: EntityBase):
 		if bullet is RedCrystalBullet:
 			bullet.baseDamage = readStore("atk")
 			bullet.radius = readStore("radius")
+			bullet.percent = readStore("percent")
+			bullet.count = readStore("count")
 	return true
