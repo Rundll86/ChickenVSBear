fef6041810cbbdf491992f1de66a6d9decb78d27
陨落基围虾 <3161880837@qq.com>
2025-12-22 13:16:27 +0800

    refactor(武器系统): 调整红水晶武器参数和世界边界碰撞
    修改红水晶武器的攻击力和数量参数，优化游戏平衡性
    将圆形碰撞形状替换为世界边界碰撞形状，改进地图边界检测
    移除调试标记并更新武器描述模板

--- scripts/Contents/Weapons/RedCrystal.gd
+++ scripts/Contents/Weapons/RedCrystal.gd
@@ -5,7 +5,7 @@ class_name RedCrystalWeapon
 func update(to: int, origin: Dictionary, _entity: EntityBase):
 	origin["atk"] += 5 * to * soulLevel
 	origin["radius"] += 1 * to * soulLevel
-	origin["count"] = 2 + 1 * soulLevel
+	origin["count"] = 1 + 1 * soulLevel
 	origin["percent"] += 0.25 * (soulLevel - 1)
 	return origin
 func attack(entity: EntityBase):
