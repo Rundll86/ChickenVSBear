b44161c4a4b4a2dcf78ba97a8cbf2a87f0065c2e
陨落基围虾 <3161880837@qq.com>
2026-01-30 12:21:02 +0800

    feat(游戏内容): 添加新饲料和调整现有饲料属性
    添加巧克力(Choclate)和百合花冠(Flower)两种新饲料资源及配置
    调整鸡蛋(Egg)和彩虹糖(RainbowCandy)的字段值和数值
    修改初始饲料选择面板逻辑，排除武器类饲料并添加关闭面板功能
    移除公鸡角色(Rooster)的旧武器并添加紫水晶武器(PurpleCrystal)

--- scripts/Contents/Panels/SelectIntialFeed.gd
+++ scripts/Contents/Panels/SelectIntialFeed.gd
@@ -6,7 +6,11 @@ extends FullscreenPanelBase
 func beforeOpen(_args: Array = []):
 	for feed in initialFeedSelection.get_children():
 		feed.queue_free()
+	ComponentManager.feeds.shuffle()
 	for feed in ComponentManager.feeds:
 		var card = feed.instantiate() as Feed
+		if card.topic == FeedName.Topic.WEAPON:
+			continue
 		card.freeToBuy = true
+		card.selected.connect(func(_success): UIState.closeCurrentPanel())
 		initialFeedSelection.add_child(card)
