0635f144ba623b8c032984443502c137a380611a
陨落基围虾 <3161880837@qq.com>
2026-03-17 06:49:46 +0800

    feat(子弹): 添加招架子弹的招架率参数
    为招架子弹添加可配置的招架率参数，允许控制子弹成功招架的概率
    修改招架特效生成位置，使其在招架时更明显
    调整测试波次数据以便于开发测试

--- scripts/Contents/Bullets/Parrier.gd
+++ scripts/Contents/Bullets/Parrier.gd
@@ -1,10 +1,12 @@
 extends BulletBase
 class_name ParrierBullet
 
+@export var parryRate: float = 1
+
 func hitBullet(bullet: BulletBase):
 	if BulletTool.canDamage(bullet, launcher):
-		var eff = EffectController.create(ComponentManager.getEffect("Parry"), position)
-		eff.modulate = bullet.modulate
-		eff.shot()
-		bullet.tryDestroy()
-		tryDestroy()
+		if MathTool.rate(parryRate):
+			var eff = EffectController.create(ComponentManager.getEffect("Parry"), position + (bullet.position - position).normalized() * 100)
+			eff.modulate = bullet.modulate
+			eff.shot()
+			bullet.tryDestroy()
