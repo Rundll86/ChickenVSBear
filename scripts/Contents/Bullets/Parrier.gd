a89c4c257224761c8b5c06eaf70cabce14cc3a91
陨落基围虾 <3161880837@qq.com>
2026-03-17 06:34:07 +0800

    refactor(Bullets): 优化Parrier子弹效果和动画
    - 调整子弹碰撞体大小和位置
    - 修改动画效果，简化动画轨道
    - 移除未使用的AI函数
    - 添加子弹颜色继承效果

--- scripts/Contents/Bullets/Parrier.gd
+++ scripts/Contents/Bullets/Parrier.gd
@@ -3,8 +3,8 @@ class_name ParrierBullet
 
 func hitBullet(bullet: BulletBase):
 	if BulletTool.canDamage(bullet, launcher):
-		EffectController.create(ComponentManager.getEffect("Parry"), position).shot()
+		var eff = EffectController.create(ComponentManager.getEffect("Parry"), position)
+		eff.modulate = bullet.modulate
+		eff.shot()
 		bullet.tryDestroy()
 		tryDestroy()
-func ai():
-	PresetBulletAI.forward(self , rotation)
